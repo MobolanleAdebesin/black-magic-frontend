c699f1cc9e5851d6446d12b21b71dea60444aef5
CENWACHUKWU <CENWACHUKWUU@GMAIL.COM>
2019-11-19 00:18:55 -0500

    finished css for creators page

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import image from "./black-pride-woman-african-american-pregnancy-afro-hair-png-transparent-images.jpg"
 import "./Home.css";
 
 
--- src/MeetTheCreators.js
+++ src/MeetTheCreators.js
@@ -1,10 +1,54 @@
 import React, { Component } from 'react';
+import Chi from "./UNADJUSTEDNONRAW_thumb_32d.jpg"
+import "./MeetTheCreators.css"
 
 class MeetTheCreators extends Component {
     render() {
         return (
-            <div className="Home">
-                <h1>this is MeetTheCreators</h1>
+            <div className="Creator-box">
+                <div className="Container">
+                    <div className="Picture">
+                        <img src={Chi} alt="" height="450px" width="270px" />
+                    </div>
+                    <div className="Profile">
+                        <div className="Profile-box">
+                            <h2>Chi Nwachukwu</h2>
+                            <p className="Text-box">Biologist and Software Engineer</p>
+                            <p className="Text-box">Black Magic is the collective power of black people</p>
+                            <p className="Text-box">Inspo: <span>I just wanted a site to look for someone who could do some bombass knotless braids</span></p>
+                            <p className="Text-box">Favs: <span>Beyonce, Perfumes, Diamonds, Chimamanda Ngozi Adichie</span></p>
+                        </div>
+                    </div>
+                </div>
+                <div className="Container">
+                    <div className="Picture">
+                        <img src={Chi} alt="" height="450px" />
+                    </div>
+                    <div className="Profile">
+                        <div className="Profile-box">
+                            <h2>Chi Nwachukwu</h2>
+                            <p className="Text-box">Biologist and Software Engineer</p>
+                            <p className="Text-box">Black Magic is the collective power of black people</p>
+                            <p className="Text-box">Inspo: <span>I just wanted a site to look for someone who could do some bombass knotless braids</span></p>
+                            <p className="Text-box">Favs: <span>Beyonce, Perfumes, Diamonds, Chimamanda Ngozi Adichie</span></p>
+                        </div>
+                    </div>
+                </div>
+                <div className="Container">
+                    <div className="Picture">
+                        <img src={Chi} alt="" height="450px" />
+                    </div>
+                    <div className="Profile">
+                        <div className="Profile-box">
+                            <h2>Chi Nwachukwu</h2>
+                            <p className="Text-box">Biologist and Software Engineer</p>
+                            <p className="Text-box">Black Magic is the collective power of black people</p>
+                            <p className="Text-box">Inspo: <span>I just wanted a site to look for someone who could do some bombass knotless braids</span></p>
+                            <p className="Text-box">Favs: <span>Beyonce, Perfumes, Diamonds, Chimamanda Ngozi Adichie</span></p>
+                        </div>
+                    </div>
+                </div>
+
             </div>
         )
     }
