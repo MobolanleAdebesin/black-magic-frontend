786b54a73e950af1f194126acd2ea1cefdbbbcce
CENWACHUKWU <CENWACHUKWUU@GMAIL.COM>
2019-11-21 16:47:19 -0500

    user authetification work with bola

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,8 +1,68 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import Axios from "axios";
 import "./Navbar.css";
+import Login from "./Login";
 
 class Navbar extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            username: "",
+            password: "",
+            isLoggedin: false
+        }
+        this.onChangeusername = this.onChangeusername.bind(this);
+        this.onChangepassword = this.onChangepassword.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+    }
+
+    componentDidMount() {
+        if (localStorage.token) {
+          this.setState({
+            isLoggedIn: true
+          });
+        } else {
+          this.setState({
+            isLoggedIn: false
+          });
+        }
+    }
+
+    onChangeusername(evt) {
+        this.setState({
+            username: evt.target.value
+        });
+    }
+    onChangepassword(evt) {
+        this.setState({
+            password: evt.target.value
+        });
+    }
+
+    onSubmit(evt) {
+        evt.preventDefault();
+        const User = {
+            username: this.state.username,
+            password: this.state.password,
+            isLoggedin: true
+        }
+        console.log(User);
+
+        Axios.post('http://localhost:4000/users/login', User)
+        .then(response => {
+            localStorage.token = response.data.token
+            console.log(response.data.token)
+            this.setState({isLoggedIn: true})
+        })
+        .catch(err => console.log(err))
+
+        this.setState({
+            username: "",
+            password: "",
+            isLoggedin: false
+        })
+    }
 
     render() {
         return (
@@ -31,10 +91,10 @@ class Navbar extends Component {
                         <Link to="/Profile">
                             <li>Profile</li>
                         </Link>
+                        <Link to="/RegisterLogin">
+                            <li>Register/Login</li>
+                        </Link>
                     </ul>
-                </div>
-                <div>
-                    <input type="text" value="" placeholder="Search..." />
                 </div>
             </div>
         )
